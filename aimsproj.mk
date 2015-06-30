c53e7b69f0bce7fd962154f6d6ab5fb247ac1319
Jan Hermann <jan@janhermann.cz>
2015-06-30 17:28:46 +0200

    a bit of refactoring, less explicit calls to make now

--- aimsproj.mk
+++ aimsproj.mk
@@ -4,24 +4,27 @@ AIMSROOT ?= ~/builds/fhi-aims
 aimsroot_remote = ~/software/fhi-aims
 
 .PHONY: all prepare run_% update remote_% upload_% check monitor_% clean distclean
+.SECONDEXPANSION:
 
 all: RUN/local_job.log
 
-RUN/%_job.log: prepare.py ${tools} run_aims.sh ${prereq}
+RUN/%_job.log: prepare.py external ${prereq}
 ifneq ("$(wildcard RUN/*.start RUN/*.running.*)", "")
 	$(error "Some jobs are still running.")
 endif
 	make prepare
 	make run_$*
 
-${tools}: %:
+${tools} aimsproj.mk:
 	rsync -a ${tooldir}/$@ $@
 
 run_aims.sh:
 	rsync -a ~/bin/$@ $@
 
 prepare:
-	-rm -r RUN
+ifneq ("$(wildcard RUN)", "")
+	$(error "There is a previous RUN, run make cleanrun to overwrite.")
+endif
 	AIMSROOT=${AIMSROOT} python prepare.py
 
 run_local:
@@ -29,22 +32,22 @@ run_local:
 
 run_%:
 	bash ~/bin/submit.sh $*.job.sh
+	@sleep 0.3  # some submitters print asynchronously
 
 update:
-	make -B ${tools} run_aims.sh
-	rsync -a ${tooldir}/aimsproj.mk aimsproj.mk
+	make -B external
 
-remote_%:
+external: ${tools} run_aims.sh aimsproj.mk
+
+remote_%: upload_$$(firstword $$(subst _, , %))
 	$(eval remote := $(firstword $(subst _, , $*)))
-	make upload_${remote}
 	ssh ${remote} "cd ${remotedir}/$(notdir ${PWD}) && AIMSROOT=${aimsroot_remote} make RUN/$*_job.log"
 
 submit_%:
 	$(eval remote := $(firstword $(subst _, , $*)))
 	ssh ${remote} "cd ${remotedir}/$(notdir ${PWD}) && make run_$*"
 
-upload_%:
-	make ${tools} run_aims.sh
+upload_%: external
 	rsync -a --delete \
 		--exclude=*.pyc --exclude=RUN --exclude=run_aims.sh \
 		--include=$*_*.job.sh --exclude=*_*.job.sh \
@@ -59,10 +62,15 @@ monitor_%:
 clean:
 	-rm *.pyc
 
-distclean: clean
+cleanrun:
 	-rm -r RUN
+
+distclean: clean cleanrun
 	-rm ${tools}
 	-rm run_aims.sh
+
+cleanrun_%:
+	ssh $* "cd ${remotedir}/$(notdir ${PWD}) && make cleanrun"
 
 distclean_%:
 	ssh $* "cd ${remotedir}/$(notdir ${PWD}) && make distclean"
