1a59e524ebd87881482d59037d1a58bffc397025
Jan Hermann <jan@janhermann.cz>
2015-07-01 03:17:51 +0200

    remove python 2/3 compatibilityy, didn't work

--- dispatcher.py
+++ dispatcher.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 from pathlib import Path
 from slugify import slugify
-from builtins import str  # python23
 import json
 from collections import namedtuple
 import cPickle as pickle
@@ -10,7 +9,7 @@ import cPickle as pickle
 def dispatch(root, tasks, preparer):
     root = Path(root)
     params = [[(k, v[1]) for k, v in t] for t in tasks]
-    paths = [root/(('_'.join(slugify(str(v)) for k, v in p) or '_') + '.start')
+    paths = [root/(('_'.join(slugify(unicode(v)) for k, v in p) or '_') + '.start')
              for p in params]
     tasks = [{k: v[0] for k, v in t} for t in tasks]
     for path, task, param in zip(paths, tasks, params):
