d50431dd4b5ecc52e5acad8d66b6780f4b2a678d
Jan Hermann <jan@janhermann.cz>
2015-07-08 02:29:54 +0200

    FIX global object where it should not have been

--- logparser.py
+++ logparser.py
@@ -5,7 +5,6 @@ from functools import wraps
 
 class Parser(object):
     def __init__(self):
-        self.results = defaultdict(dict)
         self.parsers = {}
 
     def readline(self):
@@ -15,6 +14,7 @@ class Parser(object):
         return self.line
 
     def parse(self, f):
+        self.results = defaultdict(dict)
         self.f = f
         while True:
             try:
