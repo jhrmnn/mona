ccbd0023c660e698aef2286fe229ad3a660ecf2a
Jan Hermann <jan@janhermann.cz>
2015-09-30 19:23:10 +0200

    refactor Remote as class

--- caflib/Remote.py
+++ caflib/Remote.py
@@ -2,53 +2,60 @@ import subprocess
 from pathlib import Path
 import glob
 from caflib.Logging import info, error
+import sys
 
 
-def upload(host, path):
-    info('Uploading to {}...'.format(host))
-    subprocess.check_call(['ssh', host, 'mkdir -p {}'.format(path)])
-    subprocess.check_call(['rsync',
-                           '-ia',
-                           '--exclude=.*',
-                           '--exclude=build',
-                           '--exclude=_caf',
-                           '--exclude=*.pyc',
-                           '--exclude=__pycache__',
-                           '.',
-                           '{}:{}'.format(host, path)])
+class Remote:
+    def __init__(self, host, path):
+        self.host = host
+        self.path = path
 
+    def update(self):
+        info('Updating {.host}...'.format(self))
+        subprocess.check_call(['ssh', self.host, 'mkdir -p {.path}'.format(self)])
+        subprocess.check_call(['rsync',
+                               '-ia',
+                               '--exclude=.*',
+                               '--exclude=build',
+                               '--exclude=_caf',
+                               '--exclude=*.pyc',
+                               '--exclude=__pycache__',
+                               '.',
+                               '{0.host}:{0.path}'.format(self)])
 
-def command(cmd, host, path):
-    info('Running `./caf {}` on {}...'.format(cmd, host))
-    try:
-        subprocess.check_call(['ssh', host,
-                               'cd {} && ./caf {}'.format(path, cmd)])
-    except subprocess.CalledProcessError:
-        error('Command `./caf {}` on {} ended with error'
-              .format(cmd, host))
+    def command(self, replace=None):
+        if replace:
+            cmd = ' '.join(a if a != self.host else replace for a in sys.argv)
+        else:
+            cmd = ' '.join(a for a in sys.argv if a != self.host)
+        info('Running `{}` on {.host}...'.format(cmd, self))
+        try:
+            subprocess.check_call(['ssh', self.host,
+                                   'cd {.path} && {}'.format(self, cmd)])
+        except subprocess.CalledProcessError:
+            error('Command `{}` on {.host} ended with error'
+                  .format(cmd, self))
 
+    def fetch(self, targets, cellar, batch):
+        info('Fetching from {}...'.format(self.host))
+        if targets:
+            targets = [batch/t for t in targets]
+        else:
+            targets = [Path(p) for p in glob.glob('{}/*'.format(batch))]
+        paths = set()
+        for target in targets:
+            for task in target.glob('*'):
+                task = task.resolve()
+                assert task.parts[-4] == 'Cellar'
+                paths.add('/'.join(task.parts[-3:]))
+        p = subprocess.Popen(['rsync',
+                              '-iar',
+                              '--files-from=-',
+                              '{0.host}:{0.path}/{1}'.format(self, cellar),
+                              str(cellar)],
+                             stdin=subprocess.PIPE)
+        p.communicate('\n'.join(paths).encode())
 
-def fetch(targets, cellar, build, host, path):
-    info('Downloading from {}...'.format(host))
-    if targets:
-        targets = [build/t for t in targets]
-    else:
-        targets = [Path(p) for p in glob.glob('{}/*'.format(build))]
-    paths = set()
-    for target in targets:
-        for task in target.glob('*'):
-            task = task.resolve()
-            assert task.parts[-4] == 'Cellar'
-            paths.add('/'.join(task.parts[-3:]))
-    p = subprocess.Popen(['rsync',
-                          '-iar',
-                          '--files-from=-',
-                          '{}:{}/{}'.format(host, path, cellar),
-                          str(cellar)],
-                         stdin=subprocess.PIPE)
-    p.communicate('\n'.join(paths).encode())
-
-
-def go(host, path):
-    subprocess.call(['ssh', '-t', host,
-                     'cd {} && $SHELL -l'.format(path)])
+    def go(self):
+        subprocess.call(['ssh', '-t', self.host,
+                        'cd {.path} && $SHELL -l'.format(self)])
