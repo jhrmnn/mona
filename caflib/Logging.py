4c0b68acbfd292317308c602c1207b4e44f05e46
Jan Hermann <jh@janhermann.cz>
2016-02-23 04:00:10 +0100

    implement tables for printing

--- caflib/Logging.py
+++ caflib/Logging.py
@@ -1,37 +1,76 @@
 import sys
-
-_colors = {
-    'bold': '\x1b[01;1m',
-    'red': '\x1b[01;31m',
-    'green': '\x1b[32m',
-    'yellow': '\x1b[33m',
-    'pink': '\x1b[35m',
-    'blue': '\x1b[01;34m',
-    'cyan': '\x1b[36m',
-    'grey': '\x1b[37m',
-    'normal': '\x1b[0m',
-}
+from io import StringIO
+from itertools import chain, dropwhile
 
 
-class Colored:
-    def __init__(self, x):
-        self.x = x
+class colstr(str):
+    colors = {'bold': '\x1b[01;1m',
+              'red': '\x1b[01;31m',
+              'green': '\x1b[32m',
+              'yellow': '\x1b[33m',
+              'pink': '\x1b[35m',
+              'blue': '\x1b[01;34m',
+              'cyan': '\x1b[36m',
+              'grey': '\x1b[37m',
+              'normal': '\x1b[0m'}
 
-    def __format__(self, color):
-        return _colors[color.lower()] + str(self.x) + _colors['normal']
+    def __new__(cls, s, color):
+        obj = str.__new__(cls, colstr.colors[color] + str(s) + colstr.colors['normal'])
+        obj.len = len(str(s))
+        return obj
 
-    def __str__(self):
-        return str(self.x)
+    def __len__(self):
+        return self.len
 
 
 def warn(s):
-    print(_colors['yellow'] + s + _colors['normal'])
+    print(colstr(s, 'yellow'))
 
 
 def info(s):
-    print(_colors['green'] + s + _colors['normal'])
+    print(colstr(s, 'green'))
 
 
 def error(s):
-    print(_colors['red'] + s + _colors['normal'])
+    print(colstr(s, 'red'))
     sys.exit(1)
+
+
+class TableException(Exception):
+    pass
+
+
+class Table:
+    def __init__(self, **kwargs):
+        self.rows = []
+        self.set_format(**kwargs)
+
+    def add_row(self, *row, free=False):
+        self.rows.append((free, row))
+
+    def set_format(self, sep=' ', align='>'):
+        self.sep = sep
+        self.align = align
+
+    def __str__(self):
+        col_nums = [len(row) for free, row in self.rows if not free]
+        if len(set(col_nums)) != 1:
+            raise TableException('Unequal column lengths: {}'.format(col_nums))
+        col_nums = len(next(dropwhile(lambda r: r[0], self.rows))[1])
+        cell_widths = [[len(str(cell)) for cell in row]
+                       for free, row in self.rows if not free]
+        col_widths = [max(col) for col in zip(*cell_widths)]
+        seps = (col_nums-1)*[self.sep] if not isinstance(self.sep, list) else self.sep
+        seps += ['\n']
+        aligns = col_nums*[self.align] if not isinstance(self.align, list) else self.align
+        f = StringIO()
+        for free, row in self.rows:
+            if free:
+                f.write('{}\n'.format(row[0]))
+            else:
+                cells = ('{:{align}{width}}'
+                         .format(str(cell), align=align, width=width)
+                         for cell, align, width
+                         in zip(row, aligns, col_widths))
+                f.write('{}'.format(''.join(chain.from_iterable(zip(cells, seps)))))
+        return f.getvalue()[:-1]
