91bbff92fc4d90c873157b85dd38c76f2f05c01e
Jan Hermann <jh@janhermann.cz>
2016-04-02 11:01:01 +0200

    support subdirectory calculations in aims tasks

--- caflib/Tools/aims.py
+++ caflib/Tools/aims.py
@@ -1,6 +1,6 @@
 from caflib.Tools import geomlib
 from caflib.Context import feature
-from caflib.Utils import find_program, report
+from caflib.Utils import find_program, report, cd
 from caflib.Logging import info, warn, error
 from pathlib import Path
 
@@ -16,6 +16,7 @@ def reporter():
 @feature('aims')
 def prepare_aims(task):
     aims = task.consume('aims') or 'aims'
+    aims_command = 'AIMS={} run_aims'.format(aims)
     aims_binary = find_program(aims)
     if not aims_binary:
         if aims not in _reported:
@@ -28,29 +29,38 @@ def prepare_aims(task):
     if aims not in _reported:
         _reported[aims] = (info, '{} is {}'.format(aims, aims_binary))
     geomfile = task.consume('geomfile') or 'geometry.in'
-    if Path(geomfile).exists():
-        geom = geomlib.readfile(geomfile, 'aims')
-    else:
-        error('No geometry file found')
-    species = sorted(set((a.number, a.symbol) for a in geom))
     basis = task.consume('basis')
     if not basis:
         error('No basis was specified for aims')
-    if not Path('control.in').exists():
-        error('No control file found')
     basis_root = aims_binary.parents[1]/'aimsfiles/species_defaults'/basis
-    if not basis == 'none':
-        with open('control.in') as f:
-            chunks = [f.read()]
-        Path('control.in').unlink()
-        del task.files['control.in']
-        for specie in species:
-            with (basis_root/'{0[0]:02d}_{0[1]}_default'.format(specie)).open() as f:
-                chunks.append(f.read())
-        task.store_link_text('\n'.join(chunks), 'control.in', label=True)
+    subdirs = list(task.consume('subdirs') or ['.'])
+    if subdirs[0] != '.':
+        aims_command += ' >run.out 2>run.err'
+    check_output = task.consume('check')
+    if check_output or check_output is None:
+        aims_command += ' && grep "Have a nice day" run.out >/dev/null'
+    command = []
+    for subdir in subdirs:
+        with cd(subdir):
+            if Path(geomfile).exists():
+                geom = geomlib.readfile(geomfile, 'aims')
+            else:
+                error('No geometry file found')
+            species = sorted(set((a.number, a.symbol) for a in geom))
+            if not Path('control.in').exists():
+                error('No control file found')
+            if not basis == 'none':
+                with open('control.in') as f:
+                    chunks = [f.read()]
+                Path('control.in').unlink()
+                del task.files['control.in']
+                for specie in species:
+                    with (basis_root/'{0[0]:02d}_{0[1]}_default'.format(specie)).open() as f:
+                        chunks.append(f.read())
+                task.store_link_text('\n'.join(chunks), 'control.in', label=True)
+            if subdir == '.':
+                command.append(aims_command)
+            else:
+                command.append('(cd {} && env {})'.format(subdir, aims_command))
     if 'command' not in task.attrs:
-        command = 'AIMS={} run_aims'.format(aims)
-        check_output = task.consume('check')
-        if check_output or check_output is None:
-            command += ' && grep "Have a nice day" run.out >/dev/null'
-        task.attrs['command'] = command
+        task.attrs['command'] = '\n'.join(command)
