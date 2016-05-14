efb1bfad5d0de9e89c20a59f9802e4f3751ec6be
Jan Hermann <jh@janhermann.cz>
2016-05-15 00:01:26 +0200

    refactor Worker

--- caflib/Worker.py
+++ caflib/Worker.py
@@ -1,9 +1,10 @@
 import json
 import subprocess
 from pathlib import Path
-import signal
+from signal import signal, SIGINT, SIGTERM, SIGXCPU
 import sys
 import os
+from datetime import datetime
 from abc import ABCMeta, abstractmethod
 from urllib.request import urlopen
 from urllib.error import HTTPError, URLError
@@ -16,27 +17,30 @@ from caflib.Utils import cd, Configuration
 
 
 class Worker(metaclass=ABCMeta):
+    verify_lock = True
+
     def __init__(self, myid, root, dry=False, limit=None, debug=False):
         self.myid = myid
         self.root = Path(root)
         self.dry = dry
         self.limit = limit
         self.debug = debug
-        signal.signal(signal.SIGINT, self.sigint_handler)
-        signal.signal(signal.SIGTERM, self.sigint_handler)
-        self.print_info('Alive and ready.')
-        self.verify_lock = True
+        signal(SIGINT, self.signal_handler)
+        signal(SIGTERM, self.signal_handler)
+        self.info('Alive and ready.')
 
-    def sigint_handler(self, sig, frame):
-        self.print_info('Interrupted, quitting.')
+    def signal_handler(self, sig, frame):
+        self.info('Interrupted, quitting.')
         sys.exit()
 
-    def print_info(self, msg):
-        print('Worker {}: {}'.format(self.myid, msg))
+    def info(self, msg):
+        print('Worker {}: {:%Y-%m-%d %H:%M:%S}: {}'.format(
+            self.myid, datetime.today(), msg
+        ))
 
-    def print_debug(self, msg):
+    def debug_info(self, msg):
         if self.debug:
-            print('Worker {}: {}'.format(self.myid, msg))
+            self.info(msg)
 
     @abstractmethod
     def get_task(self):
@@ -57,13 +61,13 @@ class Worker(metaclass=ABCMeta):
     def work(self):
         n_done = 0
         for label, taskid in self.locked_tasks():
-            self.print_info('Started working on {} ({})...'.format(label, taskid))
+            self.info('Started working on {} ({})...'.format(label, taskid))
             if not self.dry:
                 self.run_command(taskid)
-            self.print_info('Finished working on {}.'.format(label))
+            self.info('Finished working on {}.'.format(label))
             n_done += 1
             if self.limit and n_done >= self.limit:
-                self.print_info('Reached limit of tasks, quitting.')
+                self.info('Reached limit of tasks, quitting.')
                 break
 
     def locked_tasks(self):
@@ -73,57 +77,27 @@ class Worker(metaclass=ABCMeta):
                     return
                 yield label, taskid
 
-    def run_command(self, taskid):
-        with cd(self.root/taskid):
-            if Path('command').is_file():
-                with open('command') as f:
-                    command = f.read()
-            else:
-                command = ''
-            if Path('.caf/env').is_file():
-                command = 'source .caf/env\n' + command
-            with open('run.out', 'w') as stdout, \
-                    open('run.err', 'w') as stderr:
-                try:
-                    subprocess.check_call(command,
-                                          shell=True,
-                                          stdout=stdout,
-                                          stderr=stderr)
-                except subprocess.CalledProcessError as e:
-                    print(e)
-                    self.print_info(
-                        'error: There was an error when working on {}'.format(taskid))
-                    with Path('.caf/error').open('w') as f:
-                        f.write(self.myid + '\n')
-                    self.task_error(taskid)
-                else:
-                    if 'CAFWAIT' in os.environ:
-                        from time import sleep
-                        sleep(int(os.environ['CAFWAIT']))
-                    with Path('.caf/seal').open('w') as f:
-                        f.write(self.myid + '\n')
-                    if Path('.caf/error').is_file():
-                        Path('.caf/error').unlink()
-                    self.task_done(taskid)
-
     @contextmanager
     def get_locked_task(self):
         skipped = set()
         for label, taskid in self.tasks(skipped):
-            self.print_debug('Trying task {}...'.format(taskid))
+            self.debug_info('Trying task {}...'.format(taskid))
             taskpath = self.root/taskid
-            self.cwd = taskpath
+            self.current_taskid = taskid
             lockpath = taskpath/'.lock'
             if (taskpath/'.caf/seal').is_file():
-                self.print_debug('Task {} is sealed, continue.'.format(taskid))
+                self.debug_info('Task {} is sealed, continue.'.format(taskid))
                 self.task_done(taskid)
             elif (taskpath/'.caf/error').is_file() and self.verify_lock:
-                self.print_debug('Task {} is in error, continue.'.format(taskid))
+                self.debug_info('Task {} is in error, continue.'.format(taskid))
                 self.task_error(taskid)
-            elif not all((p/'.caf/seal').is_file() for p in get_children(taskpath)) \
-                    and not self.dry:
-                self.print_debug('Task {} has unsealed children, put back and continue.'
-                                 .format(taskid))
+            elif not all(
+                (p/'.caf/seal').is_file() for p in get_children(taskpath)
+            ) and not self.dry:
+                self.debug_info(
+                    'Task {} has unsealed children, put back and continue.'
+                    .format(taskid)
+                )
                 self.put_back(label, taskid)
                 skipped.add(taskid)
             else:
@@ -132,7 +106,7 @@ class Worker(metaclass=ABCMeta):
                 except OSError:
                     if not self.verify_lock:
                         break
-                    self.print_debug('Task {} is locked, continue.'.format(taskid))
+                    self.debug_info('Task {} is locked, continue.'.format(taskid))
                 else:
                     break  # we have acquired lock
         else:  # there is no task left
@@ -149,14 +123,46 @@ class Worker(metaclass=ABCMeta):
         while True:
             label, taskid = self.get_task()
             if taskid is None:
-                self.print_info('No more tasks in queue, quitting.')
+                self.info('No more tasks in queue, quitting.')
                 return
             elif taskid in skipped:
                 self.put_back(label, taskid)
-                self.print_info('All tasks have been skipped, quitting.')
+                self.info('All tasks have been skipped, quitting.')
                 return
             else:
                 yield label, taskid
+
+    def run_command(self, taskid):
+        with cd(self.root/taskid):
+            if Path('command').is_file():
+                with open('command') as f:
+                    command = f.read()
+            else:
+                command = ''
+            if Path('.caf/env').is_file():
+                command = 'source .caf/env\n' + command
+            with open('run.out', 'w') as stdout, open('run.err', 'w') as stderr:
+                try:
+                    subprocess.check_call(
+                        command, shell=True, stdout=stdout, stderr=stderr
+                    )
+                except subprocess.CalledProcessError as e:
+                    print(e)
+                    self.info(
+                        'error: There was an error when working on {}'.format(taskid)
+                    )
+                    with Path('.caf/error').open('w') as f:
+                        f.write(self.myid + '\n')
+                    self.task_error(taskid)
+                else:
+                    if 'CAFWAIT' in os.environ:
+                        from time import sleep
+                        sleep(int(os.environ['CAFWAIT']))
+                    with Path('.caf/seal').open('w') as f:
+                        f.write(self.myid + '\n')
+                    if Path('.caf/error').is_file():
+                        Path('.caf/error').unlink()
+                    self.task_done(taskid)
 
 
 def get_children(path):
@@ -171,27 +177,29 @@ class LocalWorker(Worker):
 
     def get_task(self):
         try:
-            path, label = self.queue.pop(0)
-            return label, path
+            taskid, label = self.queue.pop(0)
+            return label, taskid
         except IndexError:
             return None, None
 
-    def put_back(self, label, path):
-        self.queue.append((path, label))
+    def put_back(self, label, taskid):
+        self.queue.append((taskid, label))
 
-    def task_done(self, path):
+    def task_done(self, taskid):
         pass
 
-    def task_error(self, path):
+    def task_error(self, taskid):
         pass
 
 
-curl_pushover = '\
+curl_pushover = """\
 -F "token={token:}" -F "user={user:}" -F "title=Worker" -F "message={message:}" \
-https://api.pushover.net/1/messages.json >/dev/null'
+https://api.pushover.net/1/messages.json >/dev/null"""
 
 
 class QueueWorker(Worker):
+    verify_lock = False
+
     def __init__(self, myid, root, url, dry=False, limit=None, debug=False):
         super().__init__(myid, root, dry, limit, debug)
         conf = Configuration(os.environ['HOME'] + '/.config/caf/conf.yaml')
@@ -201,21 +209,23 @@ class QueueWorker(Worker):
         self.url_state = {}
         self.url_putback = {}
         self.has_warned = False
-        self.verify_lock = False
-        signal.signal(signal.SIGXCPU, self.sigxcpu_handler)
+        signal(SIGXCPU, self.signal_handler)
 
-    def sigxcpu_handler(self, sig, frame):
-        self.print_info('Will be soon interrupted.')
-        self.put_back(None, self.cwd)
-        self.call_pushover('Worker #{} on {} will be soon interrupted'
-                           .format(self.myid, socket.gethostname()))
-        self.has_warned = True
-
-    def sigint_handler(self, sig, frame):
-        self.print_info('Interrupted, quitting.')
-        if not self.has_warned:
-            self.sigxcpu_handler(sig, frame)
+    def interrupt(self):
+        self.call_pushover(
+            'Worker #{} on {} will be soon interrupted'
+            .format(self.myid, socket.gethostname())
+        )
+        self.put_back(None, self.current_taskid)
         sys.exit()
+
+    def signal_handler(self, sig, frame):
+        if not self.has_warned:
+            self.has_warned = True
+            self.info('Will be soon interrupted.')
+            self.interrupt()
+        elif not sig == SIGXCPU:
+            super().signal_handler(sig, frame)
 
     def call_url(self, url):
         if self.curl:
@@ -232,13 +242,16 @@ class QueueWorker(Worker):
         if self.curl:
             subprocess.check_call(
                 self.curl % curl_pushover.format(token=token, user=user, message=msg),
-                shell=True)
+                shell=True
+            )
         else:
             conn = HTTPSConnection('api.pushover.net:443')
-            conn.request('POST',
-                         '/1/messages.json',
-                         urlencode({'token': token, 'user': user, 'message': msg}),
-                         {'Content-type': 'application/x-www-form-urlencoded'})
+            conn.request(
+                'POST',
+                '/1/messages.json',
+                urlencode({'token': token, 'user': user, 'message': msg}),
+                {'Content-type': 'application/x-www-form-urlencoded'}
+            )
             conn.getresponse()
 
     def get_task(self):
@@ -258,19 +271,20 @@ class QueueWorker(Worker):
             except HTTPError:
                 return None, None
             except URLError as e:
-                self.print_info('error: Cannot connect to {}: {}'
-                                .format(self.url, e.reason))
+                self.info(
+                    'error: Cannot connect to {}: {}'.format(self.url, e.reason)
+                )
                 return None, None
         task, label, url_state, url_putback = response.split()
         self.url_state[task] = url_state
         self.url_putback[task] = url_putback
         return label, task
 
-    def put_back(self, label, path):
-        self.call_url(self.url_putback.pop(path))
+    def put_back(self, label, taskid):
+        self.call_url(self.url_putback.pop(taskid))
 
-    def task_done(self, path):
-        self.call_url(self.url_state.pop(path) + '?state=Done')
+    def task_done(self, taskid):
+        self.call_url(self.url_state.pop(taskid) + '?state=Done')
 
-    def task_error(self, path):
-        self.call_url(self.url_state.pop(path) + '?state=Error')
+    def task_error(self, taskid):
+        self.call_url(self.url_state.pop(taskid) + '?state=Error')
