7428eb51799ef8cac5b5085c04bd29a9a5734778
Jan Hermann <dev@janhermann.cz>
2018-01-23 16:37:23 +0100

    make sure rsync source ends with slash

--- caflib/Remote.py
+++ caflib/Remote.py
@@ -35,9 +35,9 @@ class Remote:
         if delete:
             cmd.append('--delete')
         cmd.extend(f'--exclude={patt}' for patt in exclude)
-        cmd.append(str(top))
         if os.path.exists('caflib'):
             cmd.append('caflib')
+        cmd.append(str(top) + '/')
         cmd.append(f'{self.host}:{self.path}')
         sp.run(cmd, check=True)
 
