d625eff6c7a5d717f4cc6ccbf55b9d8aee4305e3
Jan Hermann <dev@janhermann.cz>
2018-02-15 17:31:21 +0100

    rename top-level caflib to caf

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=['mypy_extensions'],
     entry_points={
         'console_scripts': [
-            'caf = caflib.Caf:main',
+            'caf = caf.Caf:main',
         ],
     }
 )
