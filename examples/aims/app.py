64091ce25b8e74f04a85f66ff1f2e495c43d9bff
Jan Hermann <dev@janhermann.cz>
2018-02-20 14:26:57 +0100

    cleanup example

--- examples/aims/app.py
+++ examples/aims/app.py
@@ -7,42 +7,34 @@ from caf.cellar import Cellar, collect
 from caf.executors import DirBashExecutor
 from caf.Tools.aims import AimsTask
 from caf.Tools.geomlib import Molecule
-from caf.scheduler import Scheduler
 
 app = Caf()
 cellar = Cellar(app)
-dir_bash = DirBashExecutor(app, cellar)
-Scheduler(cellar)
-aims = AimsTask(dir_bash)
-
-
-def parse_ene(path: Path) -> float:
-    with open(path) as f:
-        return float(
-            next(l for l in f if 'Total energy uncorrected' in l).split()[5]
-        )
-
-
-async def get_ene(dist: float) -> float:
-    outputs = await aims.task(
-        aims='aims-master',
-        basis='light',
-        tags=dict(
-            xc='pbe',
-        ),
-        geom=Molecule.from_coords(['Ar', 'Ar'], [
-            (0, 0, 0),
-            (dist, 0, 0),
-        ]),
-        label=str(dist),
-    )
-    return parse_ene(outputs['run.out'].path)
+aims = AimsTask(DirBashExecutor(app, cellar))
 
 
 @app.route('main')
 async def main() -> List[float]:
     dists = [3.3, 4, 5.2]
     return await collect((get_ene(dist) for dist in dists), nan)
+
+
+async def get_ene(dist: float) -> float:
+    outputs = await aims.task(
+        aims='aims-master',
+        tags={'xc': 'pbe'},
+        geom=Molecule.from_coords(['Ar', 'Ar'], [(0, 0, 0), (dist, 0, 0)]),
+        basis='light',
+        label=str(dist),
+    )
+    return parse_ene(outputs['run.out'].path)
+
+
+def parse_ene(path: Path) -> float:
+    with path.open() as f:
+        return float(
+            next(l for l in f if 'Total energy uncorrected' in l).split()[5]
+        )
 
 
 if __name__ == '__main__':
