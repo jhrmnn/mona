a695a32f859415614fc8980c0e767dd0cea24e11
Jan Hermann <dev@janhermann.cz>
2018-02-23 01:12:14 +0100

    cleanup argparse_cli

--- caf/argparse_cli.py
+++ caf/argparse_cli.py
@@ -9,53 +9,15 @@ from typing import (
     Any, Callable, TypeVar, List, Union, Dict, Tuple, Iterable, Optional
 )
 
-
-_T = TypeVar('_T')
 _F = TypeVar('_F', bound=Callable[..., Any])
+
+CliDef = Iterable[Tuple[str, Union[Callable[..., Any], List[Tuple[str, Any]]]]]
 
 
 class Arg:
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         self.args = args
         self.kwargs = kwargs
-
-
-_func_register: Dict[Callable[..., Any], List[Arg]] = {}
-
-
-CliDef = Iterable[Tuple[str, Union[Callable[..., Any], List[Tuple[str, Any]]]]]
-
-
-def _add_commands(parser: ArgumentParser, clidef: CliDef) -> None:
-    rows = []
-    for name, item in clidef:
-        if isinstance(item, list):
-            rows.append((
-                name,
-                '-> ' + ', '.join(subname for subname, _ in item)
-            ))
-        else:
-            rows.append((name, item.__doc__ or '?'))
-    maxlen = max(len(name) for name, _ in rows)
-    subparsers = parser.add_subparsers(
-        metavar='<command>',
-        help='Command to run',
-        title='commands',
-        description='\n'.join(
-            f'{name:<{maxlen}}   {desc}' for name, desc in rows
-        ),
-    )
-    for name, item in clidef:
-        subparser = subparsers.add_parser(
-            name,
-            formatter_class=parser.formatter_class  # type: ignore
-        )
-        if isinstance(item, list):
-            _add_commands(subparser, item)
-        else:
-            for arg in _func_register[item]:
-                subparser.add_argument(*arg.args, **arg.kwargs)
-            subparser.set_defaults(func=item)
 
 
 class CLIError(Exception):
@@ -78,10 +40,11 @@ class CLI:
             formatter_class=argparse.RawDescriptionHelpFormatter
         )
         self._commands: Optional[Dict[str, Any]] = OrderedDict()
+        self._func_register: Dict[Callable[..., Any], List[Arg]] = {}
 
     def parse(self, argv: List[str]) -> Dict[str, Any]:
         if self._commands:
-            _add_commands(self.parser, self._commands.items())
+            self._add_commands(self.parser, self._commands.items())
             self._commands = None
         if not argv:
             raise CLIError(self.parser, self.parser.format_help().strip())
@@ -97,7 +60,7 @@ class CLI:
     def add_command(self, func: Callable[..., Any], cli: List[Arg] = None,
                     name: str = None, group: str = None) -> None:
         assert self._commands is not None
-        _func_register[func] = cli or []
+        self._func_register[func] = cli or []
         if not name:
             if '_' in func.__name__:
                 group, name = func.__name__.split('_', 1)
@@ -114,3 +77,34 @@ class CLI:
             self.add_command(func, cli, name, group)
             return func
         return decorator
+
+    def _add_commands(self, parser: ArgumentParser, clidef: CliDef) -> None:
+        rows = []
+        for name, item in clidef:
+            if isinstance(item, list):
+                rows.append((
+                    name,
+                    '-> ' + ', '.join(subname for subname, _ in item)
+                ))
+            else:
+                rows.append((name, item.__doc__ or '?'))
+        maxlen = max(len(name) for name, _ in rows)
+        subparsers = parser.add_subparsers(
+            metavar='<command>',
+            help='Command to run',
+            title='commands',
+            description='\n'.join(
+                f'{name:<{maxlen}}   {desc}' for name, desc in rows
+            ),
+        )
+        for name, item in clidef:
+            subparser = subparsers.add_parser(
+                name,
+                formatter_class=parser.formatter_class  # type: ignore
+            )
+            if isinstance(item, list):
+                self._add_commands(subparser, item)
+            else:
+                for arg in self._func_register[item]:
+                    subparser.add_argument(*arg.args, **arg.kwargs)
+                subparser.set_defaults(func=item)
