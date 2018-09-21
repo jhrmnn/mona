4f689789e4da406f83a46da656c6578439cda9f1
Jan Hermann <dev@janhermann.cz>
2018-09-21 02:42:39 +0200

    add bytes and Path to json

--- caf2/json_utils.py
+++ caf2/json_utils.py
@@ -1,20 +1,34 @@
 import json
+import base64
+from pathlib import Path
 
 from typing import Any, Set, Type, Dict, Callable
 
 
 class ClassJSONEncoder(json.JSONEncoder):
+    defaults_classes = {
+        bytes: lambda b: {'bytes': base64.b64encode(b).decode()},
+        Path: lambda p: {'path': str(p)},
+    }
+
     def __init__(self, *args: Any, tape: Set[Any] = None,
                  classes: Dict[Type, Callable[[Any], Dict]] = None,
                  **kwargs: Any) -> None:
+        classes = classes or {}
         super().__init__(*args, **kwargs)
+        self._classes_tuple = tuple(classes)
+        classes = {
+            bytes: lambda b: {'bytes': base64.b64encode(b).decode()},
+            Path: lambda p: {'path': str(p)},
+            **classes
+        }
+        self._classes = classes
+        self._all_classes_tuple = tuple(classes)
         self._tape = tape
-        self._classes = classes or {}
-        self._classes_tuple = tuple(self._classes)
 
     def default(self, o: Any) -> Any:
-        if isinstance(o, self._classes_tuple):
-            if self._tape is not None:
+        if isinstance(o, self._all_classes_tuple):
+            if isinstance(o, self._classes_tuple) and self._tape is not None:
                 self._tape.add(o)
             class_ = o.__class__
             return {'__classname__': class_.__name__, **self._classes[class_](o)}
@@ -28,6 +42,11 @@ class ClassJSONDecoder(json.JSONDecoder):
         assert 'object_hook' not in kwargs
         kwargs['object_hook'] = self._my_object_hook
         super().__init__(*args, **kwargs)
+        classes = {
+            bytes: lambda dct: base64.b64decode(dct['bytes'].encode()),
+            Path: lambda dct: Path(dct['path']),
+            **(classes or {})
+        }
         self._classes = {
             class_.__name__: f for class_, f in (classes or {}).items()
         }
