be5a566c249792e5332e191b2bba89937b5b3db3
Jan Hermann <dev@janhermann.cz>
2018-09-23 23:12:36 +0200

    type JSON more accurately
    closes #1

--- caf2/json_utils.py
+++ caf2/json_utils.py
@@ -5,17 +5,41 @@ import json
 import base64
 from pathlib import Path
 
-from typing import Any, Set, Type, Dict, Callable
+from typing import Any, Set, Type, Dict, Callable, overload, Sequence, \
+    Union, cast
+from typing_extensions import Protocol
+
+
+class _JSONArray(Protocol):
+    def __getitem__(self, idx: int) -> 'JSONLike': ...
+    # hack to enforce an actual list
+    def sort(self) -> None: ...
+
+
+class _JSONDict(Protocol):
+    def __getitem__(self, key: str) -> 'JSONLike': ...
+    # hack to enforce an actual dict
+    @staticmethod
+    @overload
+    def fromkeys(seq: Sequence[Any]) -> Dict[Any, Any]: ...
+    @staticmethod
+    @overload
+    def fromkeys(seq: Sequence[Any], value: Any) -> Dict[Any, Any]: ...
+
+
+JSONLike = Union[str, int, float, bool, None, _JSONArray, _JSONDict]
+JSONConvertor = Callable[[Any], Dict[str, JSONLike]]
+JSONDeconvertor = Callable[[Dict[str, JSONLike]], Any]
 
 
 class ClassJSONEncoder(json.JSONEncoder):
-    defaults_classes = {
+    defaults_classes: Dict[Type[Any], JSONConvertor] = {
         bytes: lambda b: {'bytes': base64.b64encode(b).decode()},
         Path: lambda p: {'path': str(p)},
     }
 
     def __init__(self, *args: Any, tape: Set[Any] = None,
-                 classes: Dict[Type[Any], Callable[[Any], Dict[Any, Any]]] = None,
+                 classes: Dict[Type[Any], JSONConvertor] = None,
                  **kwargs: Any) -> None:
         classes = classes or {}
         super().__init__(*args, **kwargs)
@@ -29,34 +53,41 @@ class ClassJSONEncoder(json.JSONEncoder):
         self._all_classes_tuple = tuple(classes)
         self._tape = tape
 
-    def default(self, o: Any) -> Any:
+    def default(self, o: Any) -> JSONLike:
         if isinstance(o, self._all_classes_tuple):
             if isinstance(o, self._classes_tuple) and self._tape is not None:
                 self._tape.add(o)
-            class_ = o.__class__
-            return {'__classname__': class_.__name__, **self._classes[class_](o)}
-        return super().default(o)
+            return {
+                '__classname__': o.__class__.__name__,
+                **self._classes[o.__class__](o)
+            }
+        return cast(JSONLike, super().default(o))
+
+
+def assert_str(obj: Any) -> str:
+    assert isinstance(obj, str)
+    return obj
 
 
 class ClassJSONDecoder(json.JSONDecoder):
     def __init__(self, *args: Any,
-                 classes: Dict[Type[Any], Callable[[Dict[str, Any]], Any]] = None,
+                 classes: Dict[Type[Any], JSONDeconvertor] = None,
                  **kwargs: Any) -> None:
         assert 'object_hook' not in kwargs
         kwargs['object_hook'] = self._my_object_hook
         super().__init__(*args, **kwargs)
         classes = {
-            bytes: lambda dct: base64.b64decode(dct['bytes'].encode()),
-            Path: lambda dct: Path(dct['path']),
+            bytes: lambda dct: base64.b64decode(assert_str(dct['bytes']).encode()),
+            Path: lambda dct: Path(assert_str(dct['path'])),
             **(classes or {})
         }
         self._classes = {
             class_.__name__: f for class_, f in (classes or {}).items()
         }
 
-    def _my_object_hook(self, o: Dict[str, Any]) -> Any:
+    def _my_object_hook(self, o: Dict[str, JSONLike]) -> Any:
         try:
-            classname = o.pop('__classname__')
+            classname = assert_str(o.pop('__classname__'))
         except KeyError:
             return o
         return self._classes[classname](o)
