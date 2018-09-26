80b9fc43a2cb150ec9ebcbd9723aea9a053d8eff
Jan Hermann <dev@janhermann.cz>
2018-09-26 15:12:48 +0200

    rename in graph

--- caf2/graph.py
+++ caf2/graph.py
@@ -8,7 +8,7 @@ _T = TypeVar('_T')
 
 
 def traverse_execute(start: Iterable[_T],
-                     parents: Callable[[_T, MutableSequence[_T]], Iterable[_T]],
+                     edge_from: Callable[[_T, MutableSequence[_T]], Iterable[_T]],
                      execute: Callable[[_T], Iterable[_T]],
                      sentinel: Callable[[_T], bool] = None,
                      depth: bool = False,
@@ -20,32 +20,32 @@ def traverse_execute(start: Iterable[_T],
     traverse_queue.extend(start)
     while traverse_queue or execution_queue:
         if (eager_traverse or not execution_queue) and traverse_queue:
-            node = traverse_queue.pop() if depth else traverse_queue.popleft()
-            visited.add(node)
-            if sentinel and sentinel(node):
+            n = traverse_queue.pop() if depth else traverse_queue.popleft()
+            visited.add(n)
+            if sentinel and sentinel(n):
                 continue
-            for parent in parents(node, execution_queue):
-                if parent not in visited:
-                    traverse_queue.append(parent)
+            for m in edge_from(n, execution_queue):
+                if m not in visited:
+                    traverse_queue.append(m)
         else:
-            node = execution_queue.popleft()
-            traverse_queue.extend(execute(node))
+            n = execution_queue.popleft()
+            traverse_queue.extend(execute(n))
     return visited
 
 
 def traverse(start: Iterable[_T],
-             parents: Callable[[_T], Iterable[_T]],
+             edge_from: Callable[[_T], Iterable[_T]],
              sentinel: Callable[[_T], bool] = None,
              depth: bool = False,
              ) -> Set[_T]:
     return traverse_execute(
-        start, (lambda n, _: parents(n)), (lambda n: ()), sentinel, depth,
+        start, (lambda n, _: edge_from(n)), (lambda n: ()), sentinel, depth,
         eager_traverse=True,
     )
 
 
 def traverse_id(start: Iterable[_T],
-                parents: Callable[[_T], Iterable[_T]],
+                edge_from: Callable[[_T], Iterable[_T]],
                 sentinel: Callable[[_T], bool] = None,
                 depth: bool = False,
                 ) -> Iterable[_T]:
@@ -53,8 +53,8 @@ def traverse_id(start: Iterable[_T],
     start_id = {id(x): x for x in start}
     table.update(start_id)
 
-    def parents_id(n: int) -> Iterable[int]:
-        xs = parents(table[n])
+    def edge_from_id(n: int) -> Iterable[int]:
+        xs = edge_from(table[n])
         update = {id(x): x for x in xs}
         table.update(update)
         return update.keys()
@@ -66,5 +66,5 @@ def traverse_id(start: Iterable[_T],
     else:
         sentinel_id = None
 
-    visited_id = traverse(start_id.keys(), parents_id, sentinel_id, depth)
+    visited_id = traverse(start_id.keys(), edge_from_id, sentinel_id, depth)
     return (table[n] for n in visited_id)
