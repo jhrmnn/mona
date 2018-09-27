66aaa86202a973183edab2d86bee9597a350562b
Jan Hermann <dev@janhermann.cz>
2018-09-27 19:20:52 +0200

    add support for external file management to dir_task

--- caf2/rules.py
+++ caf2/rules.py
@@ -1,17 +1,37 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import os
 import subprocess
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import Any, Callable, TypeVar, Generic, Dict, Union
+from abc import abstractmethod
+from typing import Any, Callable, TypeVar, Generic, Dict, Union, TYPE_CHECKING
+from typing_extensions import Protocol, runtime
 
 from .utils import make_executable
 from .tasks import Task
 from .sessions import Session
+from .hashing import Hash
 from .errors import InvalidFileTarget
 
 _T = TypeVar('_T')
+HashedPathOrBytes = Union['HashedPath', bytes]
+if TYPE_CHECKING:
+    PathLike = os.PathLike[str]
+else:
+    from os import PathLike
+
+
+class HashedPath(PathLike):
+    @property
+    @abstractmethod
+    def hashid(self) -> Hash: ...
+
+
+@runtime
+class FileManager(Protocol):
+    def store_path(self, path: Path) -> HashedPath: ...
 
 
 class Rule(Generic[_T]):
@@ -50,8 +70,9 @@ def with_hook(name: str) -> Callable[[Rule[_T]], HookedRule[_T]]:
 
 @with_hook('dir_task')
 @Rule
-def dir_task(exe: bytes, inputs: Dict[str, Union[bytes, Path]]
-             ) -> Dict[str, bytes]:
+def dir_task(exe: Union[HashedPath, bytes],
+             inputs: Dict[str, Union[HashedPath, bytes, Path]]
+             ) -> Union[Dict[str, bytes], Dict[str, HashedPath]]:
     inputs = {'EXE': exe, **inputs}
     with TemporaryDirectory() as tmpdir:
         root = Path(tmpdir)
@@ -59,8 +80,8 @@ def dir_task(exe: bytes, inputs: Dict[str, Union[bytes, Path]]
         for filename, target in inputs.items():
             if isinstance(target, bytes):
                 (root/filename).write_bytes(target)
-            elif isinstance(target, Path):
-                (root/filename).symlink_to(target)
+            elif isinstance(target, (HashedPath, Path)):
+                (root/filename).symlink_to(Path(target))
             else:
                 raise InvalidFileTarget(repr(target))
         make_executable(exefile)
@@ -70,8 +91,11 @@ def dir_task(exe: bytes, inputs: Dict[str, Union[bytes, Path]]
                 [exefile], stdout=stdout, stderr=stderr, cwd=root, check=True,
             )
         outputs = {}
+        fmngr = Session.active().storage.get('dir_task:file_manager')
+        assert not fmngr or isinstance(fmngr, FileManager)
         for path in root.glob('**/*'):
             relpath = path.relative_to(root)
             if str(relpath) not in inputs and path.is_file():
-                outputs[str(relpath)] = path.read_bytes()
+                outputs[str(relpath)] = \
+                    fmngr.store_path(path) if fmngr else path.read_bytes()
     return outputs
