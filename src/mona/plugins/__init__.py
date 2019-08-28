7dcc5deb7a2eb1f2a0caa15562f4a68755043de7
Jan Hermann <dev@jan.hermann.name>
2019-08-28 12:07:49 +0200

    run black/isort on all

--- src/mona/plugins/__init__.py
+++ src/mona/plugins/__init__.py
@@ -1,6 +1,6 @@
-from .parallel import Parallel
 from .cache import Cache
 from .files import FileManager
+from .parallel import Parallel
 from .tmpdir import TmpdirManager
 
 __all__ = ['Parallel', 'Cache', 'FileManager', 'TmpdirManager']
